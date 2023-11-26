9510464e8d12f1fe77a98ce84dca2b761909bac6
Sylvester_Divine <Sylvesterdivine@outlook.com>
2023-11-26 19:50:55 +0100

    First step to hermes

---
+++ bot.py
@@ -0,0 +1,12 @@
+import os
+import telebot
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, message.text())
+
+bot.infinity_polling()
